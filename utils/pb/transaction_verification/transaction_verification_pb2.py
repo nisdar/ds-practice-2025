c678065dccc118342592a8bec97e8be95bd85378
addy <109079846+anadditivity@users.noreply.github.com>
2026-02-28 13:20:50 +0200

    add more transaction verification proto fields

--- utils/pb/transaction_verification/transaction_verification_pb2.py
+++ utils/pb/transaction_verification/transaction_verification_pb2.py
@@ -24,7 +24,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1etransaction_verification.proto\x12\x18transaction_verification\"\x86\x01\n\x13VerificationRequest\x12\x31\n\x05items\x18\x01 \x03(\x0b\x32\".transaction_verification.ItemData\x12<\n\ncreditCard\x18\x02 \x01(\x0b\x32(.transaction_verification.CreditCardData\"\'\n\x14VerificationResponse\x12\x0f\n\x07success\x18\x01 \x01(\x08\"*\n\x08ItemData\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x10\n\x08quantity\x18\x02 \x01(\t\"E\n\x0e\x43reditCardData\x12\x0e\n\x06number\x18\x01 \x01(\t\x12\x16\n\x0e\x65xpirationDate\x18\x02 \x01(\t\x12\x0b\n\x03\x63vv\x18\x03 \x01(\t\"\x1c\n\x0cHelloRequest\x12\x0c\n\x04name\x18\x01 \x01(\t\"!\n\rHelloResponse\x12\x10\n\x08greeting\x18\x01 \x01(\t2\x94\x01\n\x1eTransactionVerificationService\x12r\n\x11VerifyTransaction\x12-.transaction_verification.VerificationRequest\x1a..transaction_verification.VerificationResponse2k\n\x0cHelloService\x12[\n\x08SayHello\x12&.transaction_verification.HelloRequest\x1a\'.transaction_verification.HelloResponseb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1etransaction_verification.proto\x12\x18transaction_verification\"\xef\x02\n\x13VerificationRequest\x12\x31\n\x05items\x18\x01 \x03(\x0b\x32\".transaction_verification.ItemData\x12,\n\x04user\x18\x02 \x01(\x0b\x32\x1e.transaction_verification.User\x12<\n\ncreditCard\x18\x03 \x01(\x0b\x32(.transaction_verification.CreditCardData\x12\x32\n\x07\x63omment\x18\x04 \x01(\x0b\x32!.transaction_verification.Comment\x12@\n\x0e\x62illingAddress\x18\x05 \x01(\x0b\x32(.transaction_verification.BillingAddress\x12\x16\n\x0eshippingMethod\x18\x06 \x01(\t\x12\x14\n\x0cgiftWrapping\x18\x07 \x01(\x08\x12\x15\n\rtermsAccepted\x18\x08 \x01(\x08\"\'\n\x14VerificationResponse\x12\x0f\n\x07success\x18\x01 \x01(\x08\"*\n\x08ItemData\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x10\n\x08quantity\x18\x02 \x01(\t\"%\n\x04User\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x0f\n\x07\x63ontact\x18\x02 \x01(\t\"E\n\x0e\x43reditCardData\x12\x0e\n\x06number\x18\x01 \x01(\t\x12\x16\n\x0e\x65xpirationDate\x18\x02 \x01(\t\x12\x0b\n\x03\x63vv\x18\x03 \x01(\t\"\x1a\n\x07\x43omment\x12\x0f\n\x07\x63omment\x18\x01 \x01(\t\"[\n\x0e\x42illingAddress\x12\x0e\n\x06street\x18\x01 \x01(\t\x12\x0c\n\x04\x63ity\x18\x02 \x01(\t\x12\r\n\x05state\x18\x03 \x01(\t\x12\x0b\n\x03zip\x18\x04 \x01(\t\x12\x0f\n\x07\x63ountry\x18\x05 \x01(\t\"\x1c\n\x0cHelloRequest\x12\x0c\n\x04name\x18\x01 \x01(\t\"!\n\rHelloResponse\x12\x10\n\x08greeting\x18\x01 \x01(\t2\x94\x01\n\x1eTransactionVerificationService\x12r\n\x11VerifyTransaction\x12-.transaction_verification.VerificationRequest\x1a..transaction_verification.VerificationResponse2k\n\x0cHelloService\x12[\n\x08SayHello\x12&.transaction_verification.HelloRequest\x1a\'.transaction_verification.HelloResponseb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -32,19 +32,25 @@ _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'transaction_verification_pb
 if not _descriptor._USE_C_DESCRIPTORS:
   DESCRIPTOR._loaded_options = None
   _globals['_VERIFICATIONREQUEST']._serialized_start=61
-  _globals['_VERIFICATIONREQUEST']._serialized_end=195
-  _globals['_VERIFICATIONRESPONSE']._serialized_start=197
-  _globals['_VERIFICATIONRESPONSE']._serialized_end=236
-  _globals['_ITEMDATA']._serialized_start=238
-  _globals['_ITEMDATA']._serialized_end=280
-  _globals['_CREDITCARDDATA']._serialized_start=282
-  _globals['_CREDITCARDDATA']._serialized_end=351
-  _globals['_HELLOREQUEST']._serialized_start=353
-  _globals['_HELLOREQUEST']._serialized_end=381
-  _globals['_HELLORESPONSE']._serialized_start=383
-  _globals['_HELLORESPONSE']._serialized_end=416
-  _globals['_TRANSACTIONVERIFICATIONSERVICE']._serialized_start=419
-  _globals['_TRANSACTIONVERIFICATIONSERVICE']._serialized_end=567
-  _globals['_HELLOSERVICE']._serialized_start=569
-  _globals['_HELLOSERVICE']._serialized_end=676
+  _globals['_VERIFICATIONREQUEST']._serialized_end=428
+  _globals['_VERIFICATIONRESPONSE']._serialized_start=430
+  _globals['_VERIFICATIONRESPONSE']._serialized_end=469
+  _globals['_ITEMDATA']._serialized_start=471
+  _globals['_ITEMDATA']._serialized_end=513
+  _globals['_USER']._serialized_start=515
+  _globals['_USER']._serialized_end=552
+  _globals['_CREDITCARDDATA']._serialized_start=554
+  _globals['_CREDITCARDDATA']._serialized_end=623
+  _globals['_COMMENT']._serialized_start=625
+  _globals['_COMMENT']._serialized_end=651
+  _globals['_BILLINGADDRESS']._serialized_start=653
+  _globals['_BILLINGADDRESS']._serialized_end=744
+  _globals['_HELLOREQUEST']._serialized_start=746
+  _globals['_HELLOREQUEST']._serialized_end=774
+  _globals['_HELLORESPONSE']._serialized_start=776
+  _globals['_HELLORESPONSE']._serialized_end=809
+  _globals['_TRANSACTIONVERIFICATIONSERVICE']._serialized_start=812
+  _globals['_TRANSACTIONVERIFICATIONSERVICE']._serialized_end=960
+  _globals['_HELLOSERVICE']._serialized_start=962
+  _globals['_HELLOSERVICE']._serialized_end=1069
 # @@protoc_insertion_point(module_scope)
--- utils/pb/transaction_verification/transaction_verification_pb2.pyi
+++ utils/pb/transaction_verification/transaction_verification_pb2.pyi
@@ -7,12 +7,24 @@ from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class VerificationRequest(_message.Message):
-    __slots__ = ("items", "creditCard")
+    __slots__ = ("items", "user", "creditCard", "comment", "billingAddress", "shippingMethod", "giftWrapping", "termsAccepted")
     ITEMS_FIELD_NUMBER: _ClassVar[int]
+    USER_FIELD_NUMBER: _ClassVar[int]
     CREDITCARD_FIELD_NUMBER: _ClassVar[int]
+    COMMENT_FIELD_NUMBER: _ClassVar[int]
+    BILLINGADDRESS_FIELD_NUMBER: _ClassVar[int]
+    SHIPPINGMETHOD_FIELD_NUMBER: _ClassVar[int]
+    GIFTWRAPPING_FIELD_NUMBER: _ClassVar[int]
+    TERMSACCEPTED_FIELD_NUMBER: _ClassVar[int]
     items: _containers.RepeatedCompositeFieldContainer[ItemData]
+    user: User
     creditCard: CreditCardData
-    def __init__(self, items: _Optional[_Iterable[_Union[ItemData, _Mapping]]] = ..., creditCard: _Optional[_Union[CreditCardData, _Mapping]] = ...) -> None: ...
+    comment: Comment
+    billingAddress: BillingAddress
+    shippingMethod: str
+    giftWrapping: bool
+    termsAccepted: bool
+    def __init__(self, items: _Optional[_Iterable[_Union[ItemData, _Mapping]]] = ..., user: _Optional[_Union[User, _Mapping]] = ..., creditCard: _Optional[_Union[CreditCardData, _Mapping]] = ..., comment: _Optional[_Union[Comment, _Mapping]] = ..., billingAddress: _Optional[_Union[BillingAddress, _Mapping]] = ..., shippingMethod: _Optional[str] = ..., giftWrapping: bool = ..., termsAccepted: bool = ...) -> None: ...
 
 class VerificationResponse(_message.Message):
     __slots__ = ("success",)
@@ -28,6 +40,14 @@ class ItemData(_message.Message):
     quantity: str
     def __init__(self, name: _Optional[str] = ..., quantity: _Optional[str] = ...) -> None: ...
 
+class User(_message.Message):
+    __slots__ = ("name", "contact")
+    NAME_FIELD_NUMBER: _ClassVar[int]
+    CONTACT_FIELD_NUMBER: _ClassVar[int]
+    name: str
+    contact: str
+    def __init__(self, name: _Optional[str] = ..., contact: _Optional[str] = ...) -> None: ...
+
 class CreditCardData(_message.Message):
     __slots__ = ("number", "expirationDate", "cvv")
     NUMBER_FIELD_NUMBER: _ClassVar[int]
@@ -37,6 +57,26 @@ class CreditCardData(_message.Message):
     expirationDate: str
     cvv: str
     def __init__(self, number: _Optional[str] = ..., expirationDate: _Optional[str] = ..., cvv: _Optional[str] = ...) -> None: ...
+
+class Comment(_message.Message):
+    __slots__ = ("comment",)
+    COMMENT_FIELD_NUMBER: _ClassVar[int]
+    comment: str
+    def __init__(self, comment: _Optional[str] = ...) -> None: ...
+
+class BillingAddress(_message.Message):
+    __slots__ = ("street", "city", "state", "zip", "country")
+    STREET_FIELD_NUMBER: _ClassVar[int]
+    CITY_FIELD_NUMBER: _ClassVar[int]
+    STATE_FIELD_NUMBER: _ClassVar[int]
+    ZIP_FIELD_NUMBER: _ClassVar[int]
+    COUNTRY_FIELD_NUMBER: _ClassVar[int]
+    street: str
+    city: str
+    state: str
+    zip: str
+    country: str
+    def __init__(self, street: _Optional[str] = ..., city: _Optional[str] = ..., state: _Optional[str] = ..., zip: _Optional[str] = ..., country: _Optional[str] = ...) -> None: ...
 
 class HelloRequest(_message.Message):
     __slots__ = ("name",)
